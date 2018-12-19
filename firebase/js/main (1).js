b13248148054928d232556e22e13988215e26f4e
baboom3126 <km101km@gmail.com>
2018-12-19 17:23:53 +0800

    ????

---
+++ firebase/js/main (1).js
@@ -0,0 +1,11 @@
+
+
+  var config = {
+    apiKey: "AIzaSyDkeqqfLVS6owmqEQTJRo3SQ5uYeCIblF4",
+    authDomain: "nchudfll2018-eb03a.firebaseapp.com",
+    databaseURL: "https://nchudfll2018-eb03a.firebaseio.com",
+    projectId: "nchudfll2018-eb03a",
+    storageBucket: "nchudfll2018-eb03a.appspot.com",
+    messagingSenderId: "750573708430"
+  };
+  firebase.initializeApp(config);
